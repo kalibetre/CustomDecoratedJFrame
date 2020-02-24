9caf7f85cfd7127e0310b9f520c6045f9df78c1c
Kalkidan Betre <kalbetre.dev@outlook.com>
2020-02-24 10:08:41 +0300

    Apache 2.0 license

--- src/com/panther/ui/customdecoration/CustomDecorationParameters.java
+++ src/com/panther/ui/customdecoration/CustomDecorationParameters.java
@@ -1,3 +1,4 @@
+// Copyright 2020 Kalkidan Betre Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.panther.ui.customdecoration;
 
 import java.util.concurrent.atomic.AtomicInteger;
