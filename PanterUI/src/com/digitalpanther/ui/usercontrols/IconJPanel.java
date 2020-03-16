a717c3867b39457c0f05ba6017cefd9f2e598ea4
Kalkidan Betre <kalbetre.dev@outlook.com>
2020-03-16 13:55:40 +0300

    Demo App now uses the Modified CustomJPanel. (IconJPanel for previous commit)

---
+++ PanterUI/src/com/digitalpanther/ui/usercontrols/IconJPanel.java
@@ -0,0 +1,27 @@
+// Copyright 2020 Kalkidan Betre Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.digitalpanther.ui.usercontrols;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class IconJPanel extends JPanel {
+   private Image image;
+
+   public IconJPanel(Image image){
+      if(image != null){
+         this.image = image;
+         Dimension size = new Dimension(image.getWidth(null), image.getHeight(null));
+         setPreferredSize(size);
+         setMinimumSize(size);
+         setMaximumSize(size);
+         setSize(size);
+         setLayout(null);
+      }
+   }
+
+   @Override
+   public void paintComponent(Graphics g){
+      if(image != null)
+         g.drawImage(image, 0, 0, null);
+   }
+}
--- PanterUIDemo/src/com/digitalpanther/ui/demo/DemoApp.java
+++ PanterUIDemo/src/com/digitalpanther/ui/demo/DemoApp.java
@@ -1,16 +1,13 @@
 // Copyright 2020 Kalkidan Betre Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.digitalpanther.ui.demo;
 
-import com.digitalpanther.ui.CustomJFrame;
+import com.digitalpanther.ui.customjframe.CustomJFrame;
+import com.digitalpanther.ui.customjframe.WindowFrameType;
 import com.digitalpanther.ui.theme.Theme;
-import com.digitalpanther.ui.usercontrols.ButtonType;
-import com.digitalpanther.ui.usercontrols.ControlBoxJButton;
-import com.digitalpanther.ui.customdecoration.CustomDecorationParameters;
 import com.digitalpanther.ui.theme.DarkTheme;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class DemoApp {
    public static void main(String[] args) {
@@ -24,106 +21,37 @@ public class DemoApp {
             }
 
             final Theme darkTheme = new DarkTheme();
-            CustomJFrame frame = new CustomJFrame(darkTheme ,"Custom Decorated Window");
-            frame.setLayout(new BorderLayout());
-
-            JPanel controlBox = new JPanel();
-            controlBox.setLayout(new GridLayout(1, 3, -1, 0));
-            controlBox.setOpaque(false);
-            setupFrameControlBox(frame, controlBox, darkTheme);
-
-            JPanel titleBar = new JPanel();
-            titleBar.setLayout(new BorderLayout());
-            titleBar.setOpaque(false);
-            titleBar.add(controlBox, BorderLayout.EAST);
-
-            JPanel frameContentPane = new JPanel();
-            frameContentPane.setLayout(new BorderLayout());
-            frameContentPane.setOpaque(false);
-            frameContentPane.add(titleBar, BorderLayout.NORTH);
-
-            JPanel clientContentPane = new JPanel();
-            clientContentPane.setLayout(new FlowLayout());
-            clientContentPane.setOpaque(false);
-            frameContentPane.add(clientContentPane);
-
-            frame.setContentPane(frameContentPane);
-            frame.setBackground(darkTheme.getDefaultBackgroundColor());
+            CustomJFrame frame = new CustomJFrame(darkTheme ,"Custom Decorated Window", WindowFrameType.NORMAL);
             frame.setMinimumSize(new Dimension(600, 400));
-            frame.addComponentListener(new ComponentAdapter() {
-               @Override
-               public void componentResized(ComponentEvent e) {
-                  super.componentResized(e);
-                  // TODO: Set the Necessary Widths
-                  CustomJFrame mainFrame = (CustomJFrame) e.getSource();
-                  if (mainFrame.getExtendedState() == JFrame.MAXIMIZED_BOTH) {
-                     CustomDecorationParameters.setMaximizedWindowFrameThickness(12);
-                     mainFrame.getRootPane().setBorder(BorderFactory.createLineBorder(mainFrame.getBackground(),
-                             CustomDecorationParameters.getMaximizedWindowFrameThickness()));
-                     if(mainFrame.getRestoreButton() != null){
-                        mainFrame.getRestoreButton().setControlBoxButtonType(ButtonType.RESTORE);
-                     }
-                  } else {
-                     CustomDecorationParameters.setMaximizedWindowFrameThickness(0);
-                     mainFrame.getRootPane().setBorder(BorderFactory.createLineBorder(darkTheme.getFrameBorderColor(),
-                             CustomDecorationParameters.getFrameBorderThickness()));
-                     if(mainFrame.getRestoreButton() != null && mainFrame.getRestoreButton().controlBoxButtonType != ButtonType.MAXIMIZE){
-                        mainFrame.getRestoreButton().setControlBoxButtonType(ButtonType.MAXIMIZE);
-                     }
-                  }
-               }
-            });
-            frame.pack();
-            CustomDecorationParameters.setControlBoxWidth(controlBox.getWidth() + 10);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
+
+            JMenuBar jMenuBar = new JMenuBar();
+            JMenu fileMenu = new JMenu("File");
+            JMenu editMenu = new JMenu("Edit");
+            JMenu viewMenu = new JMenu("View");
+
+            JMenuItem openMenu = new JMenuItem("Open");
+            JMenuItem closeMenu = new JMenuItem("Close");
+            fileMenu.add(openMenu);
+            fileMenu.add(closeMenu);
+
+            JMenuItem copyMenu = new JMenuItem("Copy");
+            editMenu.add(copyMenu);
+
+            JMenuItem toolsMenu = new JMenuItem("Tools");
+            viewMenu.add(toolsMenu);
+
+            jMenuBar.add(fileMenu);
+            jMenuBar.add(editMenu);
+            jMenuBar.add(viewMenu);
+            frame.addUserControlsToTitleBar(jMenuBar);
+
+            ImageIcon imageIcon = new ImageIcon("resources/appicon.png");
+            frame.setIcon(imageIcon.getImage());
+
          }
       });
    }
-
-   private static void setupFrameControlBox(CustomJFrame frame, JPanel controlBox, Theme theme){
-      ControlBoxJButton minimizeBtn = new ControlBoxJButton(ButtonType.MINIMIZE, theme);
-      minimizeBtn.setPreferredSize(new Dimension(50, CustomDecorationParameters.getTitleBarHeight()));
-      minimizeBtn.setBackground(theme.getDefaultBackgroundColor());
-      minimizeBtn.addMouseListener(new MouseAdapter() {
-         @Override
-         public void mouseClicked(MouseEvent e) {
-            frame.setExtendedState(JFrame.ICONIFIED);
-         }
-      });
-
-      ControlBoxJButton maximizeBtn = new ControlBoxJButton(ButtonType.MAXIMIZE, theme);
-      maximizeBtn.setPreferredSize(new Dimension(50, 27));
-      maximizeBtn.setBackground(theme.getDefaultBackgroundColor());
-      maximizeBtn.addMouseListener(new MouseAdapter() {
-         @Override
-         public void mouseClicked(MouseEvent e) {
-            if (frame.getExtendedState() == JFrame.MAXIMIZED_BOTH)
-               frame.setExtendedState(JFrame.NORMAL);
-            else
-               frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-         }
-      });
-
-      ControlBoxJButton closeBtn = new ControlBoxJButton(ButtonType.CLOSE,theme);
-      closeBtn.setPreferredSize(new Dimension(50, 27));
-      closeBtn.setBackground(theme.getDefaultBackgroundColor());
-      closeBtn.addMouseListener(new MouseAdapter() {
-         @Override
-         public void mouseClicked(MouseEvent e) {
-            int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit the app ?");
-            if (response == JOptionPane.OK_OPTION) {
-               System.exit(0);
-            }
-         }
-      });
-
-      controlBox.add(minimizeBtn);
-      controlBox.add(maximizeBtn);
-      controlBox.add(closeBtn);
-
-      frame.setRestoreButton(maximizeBtn);
-   }
-
 }
 
